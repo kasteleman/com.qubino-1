9437e36162251b2c09fe1241331ebee751860049
Robin Bolscher <robinbolscher@gmail.com>
2016-09-29 14:52:12 +0200

    Cleanup ZMNHVD1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ﻿# Qubino
 
-This app adds support following Qubino devices in Homey:
+This app adds support for the following Qubino devices in Homey:
 
 * ZMNHVD1 Flush Dimmer 0-10V (Z-Wave Plus)
 * ZMNHDD1 Flush Dimmer (Z-Wave Plus)
@@ -12,9 +12,9 @@ Temperature sensor only working for ZMNHDA2.
 
 Version 1.04
 
-* Icons for devices changed
-* Powermeasurement in kWh and Watt added.
-* Moved to mobile devicecards. Be aware, not al values are visible because at this moment there is no scrollbar and items in Chrome are limited to 4 and in IOS to 3.
-* ZMNHDA2 Flush Dimmer: If no temperaturesensor connected, -999,90 °C is shown as value.
-* ZMNHVD1, ZMNHDD1 & ZMNHDA2 Flow trigger added for Temperature changed.
-* Added option in capability in case there is no temperaturesensor connected to prevent drivercrash (not tested yet)
+* Icons for devices changed.
+* Power measurement in kWh and Watt added.
+* Moved to mobile device cards. Be aware, not al values are visible because at this moment there is no scrollbar and items in Chrome are limited to 4 and in IOS to 3.
+* ZMNHDA2 Flush Dimmer: if no temperature sensor connected, -999,90 °C is shown as value.
+* ZMNHVD1, ZMNHDD1 & ZMNHDA2: flow trigger added for temperature changed.
+* Added option in capability in case there is no temperature sensor connected to prevent crash
--- drivers/ZMNHVD1/driver.js
+++ drivers/ZMNHVD1/driver.js
@@ -1,129 +1,107 @@
 'use strict';
 
-const path			= require('path');
-const ZwaveDriver	= require('homey-zwavedriver');
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
-	debug: true,
 	capabilities: {
 
 		onoff: {
 			command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
 			command_get: 'SWITCH_MULTILEVEL_GET',
 			command_set: 'SWITCH_MULTILEVEL_SET',
-			command_set_parser: function (value) {
-
-				return {
-					Value: (value > 0) ? 'on/enable' : 'off/disable',
-					'Dimming Duration': 10,
-				};
-			},
+			command_set_parser: value => ({
+				Value: (value > 0) ? 'on/enable' : 'off/disable',
+				'Dimming Duration': 10,
+			}),
 			command_report: 'SWITCH_MULTILEVEL_REPORT',
-			command_report_parser: report => {
-				console.log(JSON.stringify(report));
-				return report['Value (Raw)'][0] > 0;
-			},
+			command_report_parser: report => report['Value (Raw)'][0] > 0,
 		},
 
 		dim: {
 			command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
 			command_get: 'SWITCH_MULTILEVEL_GET',
 			command_set: 'SWITCH_MULTILEVEL_SET',
-			command_set_parser: function (value) {
+			command_set_parser: value => {
+				if (value >= 1) value = 0.99;
+
 				return {
 					Value: value * 100,
 					'Dimming Duration': 10,
 				};
 			},
 			command_report: 'SWITCH_MULTILEVEL_REPORT',
-			command_report_parser: report => {
-				console.log(JSON.stringify(report));
-				return report['Value (Raw)'][0] / 100;
-			},
+			command_report_parser: report => report['Value (Raw)'][0] / 100,
 		},
 
 		measure_temperature: {
 			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
 			command_get: 'SENSOR_MULTILEVEL_GET',
-			command_get_parser: function () {
-				return {
-					'Sensor Type': 'Temperature (version 1)',
-					Properties1: {
-						Scale: 0,
-					},
-				};
-			},
+			command_get_parser: () => ({
+				'Sensor Type': 'Temperature (version 1)',
+				Properties1: {
+					Scale: 0,
+				},
+			}),
 			command_report: 'SENSOR_MULTILEVEL_REPORT',
 			command_report_parser: report => report['Sensor Value (Parsed)'],
 			optional: true,
 		},
+
+		measure_power: {
+			command_class: 'COMMAND_CLASS_POWERLEVEL',
+			command_get: 'POWERLEVEL_GET',
+			command_get_parser: () => ({
+				'Sensor Type': 'Temperature (version 1)',
+				Properties1: {
+					Scale: 0,
+				},
+			}),
+			command_report: 'POWERLEVEL_REPORT',
+			command_report_parser: report => report['Power level (Raw)'][0],
+		}
 	},
 
 	settings: {
-		Input_1_type: {
+		input_1_type: {
 			index: 1,
 			size: 1,
 		},
-		Automatic_turning_off_output_after_set_time: {
+		automatic_turning_off_output_after_set_time: {
 			index: 11,
 			size: 2,
 		},
-		Automatic_turning_on_output_after_set_time: {
+		automatic_turning_on_output_after_set_time: {
 			index: 12,
 			size: 2,
 		},
-		State_of_device_after_power_failure: {
+		state_of_device_after_power_failure: {
 			index: 30,
 			size: 1,
-			parser: input => new Buffer([(input === true) ? 1 : 0]),
 		},
-		Maximum_dimming_value: {
+		maximum_dimming_value: {
 			index: 61,
 			size: 1,
 		},
-		Minimum_dimming_value: {
+		minimum_dimming_value: {
 			index: 60,
 			size: 1,
 		},
-		Dimming_time_soft_on_off: {
+		dimming_time_soft_on_off: {
 			index: 65,
 			size: 1,
 		},
-		Dimming_time_when_key_pressed: {
+		dimming_time_when_key_pressed: {
 			index: 66,
 			size: 1,
 		},
-		Temperature_sensor_offset: {
+		temperature_sensor_offset: {
 			index: 110,
 			size: 2,
 		},
-		Digital_temperature_sensor_reporting: {
+		digital_temperature_sensor_reporting: {
 			index: 120,
 			size: 2,
 		},
 	},
-});
-
-// bind Flow
-module.exports.on('initNode', (token) => {
-
-	const node = module.exports.nodes[token];
-	if (node) {
-		node.instance.CommandClass.COMMAND_CLASS_SENSOR_MULTILEVEL.on('report', (command, report) => {
-			if (command.name === 'SENSOR_MULTILEVEL_REPORT') {
-				console.log('Flow: New report value came in');
-				console.log(node.device_data);
-				console.log(report['Sensor Value (Parsed)']);
-				const trigger = 'ZMNHVD1_temp_changed';
-				const state = report['Sensor Value (Parsed)'];
-				const tokens = { ZMNHVD1_temp: report['Sensor Value (Parsed)'] };
-				Homey.manager('flow').triggerDevice(trigger, tokens, state, node.device_data);
-			}
-		});
-	}
-});
-
-Homey.manager('flow').on('trigger.ZMNHVD1_temp_changed', (callback) => {
-	callback(null, true);
-	return;
 });
