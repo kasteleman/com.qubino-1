03eb007824c0080fca910cdfcd104e3d320860fc
kasteleman <kasteleman@gmail.com>
2016-10-05 21:33:35 +0200

    FIxes for ZMNHDND1

--- drivers/ZMNHND1/driver.js
+++ drivers/ZMNHND1/driver.js
@@ -20,48 +20,6 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_report_parser: report => report['Value'] === 'on/enable'
 		},
 
-		measure_power: {
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: function () {
-				return {
-					Properties1: {
-						Scale: 7,
-					},
-				};
-			},
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2')
-					&& report.Properties2.hasOwnProperty('Scale bits 10')
-					&& report.Properties2['Scale bits 10'] === 2) {
-					return report['Meter Value (Parsed)'];
-				}
-				return null;
-			},
-		},
-
-		meter_power: {
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: function () {
-				return {
-					Properties1: {
-						Scale: 0,
-					},
-				};
-			},
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2')
-					&& report.Properties2.hasOwnProperty('Scale bits 10')
-					&& report.Properties2['Scale bits 10'] === 0) {
-					return report['Meter Value (Parsed)'];
-				}
-				return null;
-			},
-		},
-
 		measure_temperature: {
 			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
 			command_get: 'SENSOR_MULTILEVEL_GET',
@@ -86,14 +44,6 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 		Input_2_type: {
 			index: 1,
-			size: 1,
-		},
-		Input_2_contact_type: {
-			index: 3,
-			size: 1,
-		},
-		Input_3_contact_type: {
-			index: 4,
 			size: 1,
 		},
 		DeActivate_ALL_ON__ALL_OFF: {
