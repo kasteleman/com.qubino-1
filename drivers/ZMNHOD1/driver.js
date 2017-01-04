b0950cea19f5d8245cc80710478266a336e2199a
Carlo Landmeter <clandmeter@gmail.com>
2017-01-04 12:55:42 +0000

    ZMNHOD1: add parser logic from fibaro shutter

--- drivers/ZMNHOD1/driver.js
+++ drivers/ZMNHOD1/driver.js
@@ -10,12 +10,49 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_class: 'COMMAND_CLASS_SWITCH_BINARY',
 			command_get: 'SWITCH_BINARY_GET',
 			command_set: 'SWITCH_BINARY_SET',
+			command_set_parser: (value, node) => {
+				let result = 'off/disable';
+
+				// Check correct counter value in case of idle
+				if (value === 'idle') {
+					if (node.state.position === 'on/enable') result = 'off/disable';
+					else if (node.state.position === 'off/disable') result = 'on/enable';
+				} else if (value === 'up') {
+					result = 'on/enable';
+				}
+
+				// Save latest known position state
+				if (node && node.state) {
+					node.state.position = result;
+				}
+
+				return {
+					'Switch Value': result
+				};
+			},
 			command_report: 'SWITCH_BINARY_REPORT',
+			command_report_parser: (report, node) => {
+
+				// Save latest known position state
+				if (node && node.state) {
+					node.state.position = report['Value']
+				}
+
+				switch (report['Value']) {
+					case 'on/enable':
+						return 'up';
+					case 'off/disable':
+						return 'down';
+					default:
+						return 'idle';
+				}
+			}
 		},
 		measure_power: {
 			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
 			command_get: 'SENSOR_MULTILEVEL_GET',
 			command_report: 'SENSOR_MULTILEVEL_REPORT',
+			command_report_parser: report => report['Sensor Value (Parsed)']
 		}
 	},
 	settings: {
