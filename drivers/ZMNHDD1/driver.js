eccb25609075050aed121033b1a326d1c6307b2b
Robin Bolscher <robinbolscher@gmail.com>
2017-01-11 14:28:05 +0100

    add npm installation, remove submodules, update zwavedriver

--- drivers/ZMNHDD1/driver.js
+++ drivers/ZMNHDD1/driver.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const path = require('path');
-const ZwaveDriver = require('node-homey-zwavedriver');
+const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
+	debug: true,
 	capabilities: {
 		onoff: {
 			command_class: 'COMMAND_CLASS_SWITCH_BINARY',
